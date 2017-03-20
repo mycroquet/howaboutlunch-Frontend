dc4dd7e70ec5c27a6ff0947f91ecdf7b629ce706
Michael Roque <mikeroque1@gmail.com>
2017-03-20 17:31:01 -0600

    drop works

--- app/place-list/place-list.controller.js
+++ app/place-list/place-list.controller.js
@@ -6,23 +6,59 @@
             .module('howaboutlunch')
             .controller('placeListController', placeListController)
 
-        placeListController.$inject = ['$http']
+        placeListController.$inject = ['$http','$scope']
 
-        function placeListController($http) {
+        function placeListController($http, $scope) {
             const vm = this
 
             vm.$onInit = function() {
-              console.log('WHAAAAAT');
+              console.log($scope);
                 $http.get('http://localhost:4000/places?latitude=39.7639175&longitude=-105.0178755&term=food')
                     .then(function(response) {
-                      vm.places = response.data.businesses
-                      console.log(response.data.businesses);
+                        vm.places = response.data.businesses
+                        console.log(response.data.businesses);
                     })
-                    vm.options = {
-                      'ui-floating': true,
-                      connectWith: '.polls-container'
-                    }
-            }
-        }
 
-})();
+                $scope.dropzoneFields = []
+                $scope.dragging = false
+                vm.currentDragging;
+
+                vm.setCurrentDragging = function(place){
+                  console.log(place);
+                  vm.currentDragging = place;
+                }
+
+                vm.options = {
+                    'ui-floating': true,
+                    connectWith: '.dropzone',
+                    start: function(e, ui) {
+                        $scope.$apply(function() {
+                          console.log('DRAGGGG');
+                            $scope.dragging = true
+                        });
+                        $('.dropzone').sortable('refresh');
+                    },
+                    update: function(e, ui) {
+                        if (ui.item.sortable.droptarget[0].classList[0] !== "dropzone")
+                            ui.item.sortable.cancel();
+                    },
+                    stop: function(e, ui) {
+                      console.log(ui.item.sortable);
+                        if (ui.item.sortable.droptarget == undefined) {
+
+                            $scope.$apply($scope.dragging = false);
+                            return;
+                        } else if (ui.item.sortable.droptarget[0].classList[0] == "dropzone") {
+                            // run code when item is dropped in the dropzone
+                            console.log('ELSE TRiggered');
+                            $scope.dropzoneFields.push(vm.currentDragging)
+                            $scope.$apply($scope.dragging = false);
+                        } else {
+                            $scope.$apply($scope.dragging = false);
+                        }
+                    }
+                };
+            }
+          }
+
+        })();
