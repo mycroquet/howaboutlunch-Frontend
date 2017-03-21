cad803694446bc447a34cfff4a15ecb579afb80e
Michael Roque <mikeroque1@gmail.com>
2017-03-20 20:24:31 -0600

    fine tuning drag/drop

--- app/place-list/place-list.controller.js
+++ app/place-list/place-list.controller.js
@@ -12,7 +12,6 @@
             const vm = this
 
             vm.$onInit = function() {
-              console.log($scope);
                 $http.get('http://localhost:4000/places?latitude=39.7639175&longitude=-105.0178755&term=food')
                     .then(function(response) {
                         vm.places = response.data.businesses
@@ -22,18 +21,17 @@
                 $scope.dropzoneFields = []
                 $scope.dragging = false
                 vm.currentDragging;
+                console.log($scope.dropzoneFields);
 
                 vm.setCurrentDragging = function(place){
-                  console.log(place);
                   vm.currentDragging = place;
                 }
 
-                vm.options = {
+                vm.draggable = {
                     'ui-floating': true,
                     connectWith: '.dropzone',
                     start: function(e, ui) {
                         $scope.$apply(function() {
-                          console.log('DRAGGGG');
                             $scope.dragging = true
                         });
                         $('.dropzone').sortable('refresh');
@@ -43,12 +41,11 @@
                             ui.item.sortable.cancel();
                     },
                     stop: function(e, ui) {
-                      console.log(ui.item.sortable);
                         if (ui.item.sortable.droptarget == undefined) {
 
                             $scope.$apply($scope.dragging = false);
                             return;
-                        } else if (ui.item.sortable.droptarget[0].classList[0] == "dropzone") {
+                        } else if (ui.item.sortable.droptarget[0].classList == "dropzone") {
                             // run code when item is dropped in the dropzone
                             console.log('ELSE TRiggered');
                             $scope.dropzoneFields.push(vm.currentDragging)
@@ -58,6 +55,7 @@
                         }
                     }
                 };
+                
             }
           }
 
